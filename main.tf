bb8d46ffc34d1bb2aa581d8d0e3d20fd673b7ab9
Bong Nguyen <bong.nguyen@zoi.tech>
2024-12-29 16:13:43 +0700

    add azure storage account as backend

--- main.tf
+++ main.tf
@@ -18,12 +18,12 @@ terraform {
     # ######################
     # # BACKEND FOR .TFSTATE
     # ######################
-    # backend "azurerm" {
-    #   resource_group_name  = "app-grp"
-    #   storage_account_name = "tfstatelab29nov25"
-    #   container_name       = "tfstate"
-    #   key                  = "terraform.tfstate"
-    # }
+    backend "azurerm" {
+      resource_group_name  = "app-grp"
+      storage_account_name = "tfstatelab29nov25"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+    }
 }
 
 # Provider Block
