e17653b5de0a073f9c5ed3255e298046bc820053
Bong Nguyen <bong.nguyen@zoi.tech>
2024-12-29 15:36:00 +0700

    add virtual network

---
+++ network.tf
@@ -0,0 +1,22 @@
+resource "azurerm_virtual_network" "appnetwork" {
+  name                = local.virtual_network.name
+  location            = local.location
+  resource_group_name = data.azurerm_resource_group.app-rg.name
+  address_space       = [local.virtual_network.address_space]
+}
+
+
+resource "azurerm_subnet" "subnets" {
+  count                = var.number_of_subnet
+  name                 = "subnet${count.index}"
+  resource_group_name  = data.azurerm_resource_group.app-rg.name
+  virtual_network_name = azurerm_virtual_network.appnetwork.name
+  address_prefixes     = ["10.0.${count.index}.0/24"]
+}
+
+resource "azurerm_subnet" "bastionsubnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = data.azurerm_resource_group.app-rg.name
+  virtual_network_name = azurerm_virtual_network.appnetwork.name
+  address_prefixes     = ["10.0.100.0/24"]
+}
