81a8e87eca924b79808ffe6a577d3d734228fa35
Bong Nguyen <bong.nguyen@zoi.tech>
2025-01-03 21:37:54 +0700

    Deploy  resources in Pay-As-You-Go Subscription

--- bastion.tf
+++ bastion.tf
@@ -2,22 +2,22 @@
 #*             RESOURCES - Bastation Hosts               *#
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
 
-resource "azurerm_public_ip" "bastionpip" {
-  name                = "bastion-pubip"
-  resource_group_name = data.azurerm_resource_group.app-rg.name
-  location            = local.location
-  allocation_method   = "Static"
-  sku = "Standard"
-}
+# resource "azurerm_public_ip" "bastionpip" {
+#   name                = "bastion-pubip"
+#   resource_group_name = data.azurerm_resource_group.app-rg.name
+#   location            = local.location
+#   allocation_method   = "Static"
+#   sku = "Standard"
+# }
 
-resource "azurerm_bastion_host" "appbastion" {
-  name                = "app-bastion"
-  location            = local.location
-  resource_group_name = data.azurerm_resource_group.app-rg.name
+# resource "azurerm_bastion_host" "appbastion" {
+#   name                = "app-bastion"
+#   location            = local.location
+#   resource_group_name = data.azurerm_resource_group.app-rg.name
 
-  ip_configuration {
-    name                 = "configuration"
-    subnet_id            = azurerm_subnet.bastionsubnet.id
-    public_ip_address_id = azurerm_public_ip.bastionpip.id
-  }
-}
+#   ip_configuration {
+#     name                 = "configuration"
+#     subnet_id            = azurerm_subnet.bastionsubnet.id
+#     public_ip_address_id = azurerm_public_ip.bastionpip.id
+#   }
+# }
